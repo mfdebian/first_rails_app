4822c71268da72a9a8a894a4957d62b2fbf02d64
Alfredo González <alfredo.arosteguy@gmail.com>
2024-07-14 15:14:01 -0400

    items name uniqueness

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   has_many :traders
 
-  validates :name, presence: true, length: { minimum: 1 }
+  validates :name, presence: true, length: { minimum: 1 }, uniqueness: true
   validates :price, presence: true
 end
